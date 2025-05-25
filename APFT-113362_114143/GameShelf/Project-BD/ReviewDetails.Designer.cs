cc69e01ba0a21b6bd99c57b6b186953a9ed0f393
tito-04 <tiagojcm04@ua.pt>
2025-05-25 15:49:58 +0100

    Ja da para dar delete de uma review

--- APFT-113362_114143/GameShelf/Project-BD/ReviewDetails.Designer.cs
+++ APFT-113362_114143/GameShelf/Project-BD/ReviewDetails.Designer.cs
@@ -24,79 +24,88 @@
             this.listReactions = new System.Windows.Forms.ListView();
             this.btnBack = new System.Windows.Forms.Button();
             this.btnAddReaction = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblGameTitle
             // 
             this.lblGameTitle.AutoSize = true;
             this.lblGameTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
-            this.lblGameTitle.Location = new System.Drawing.Point(20, 20);
+            this.lblGameTitle.Location = new System.Drawing.Point(30, 31);
+            this.lblGameTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblGameTitle.Name = "lblGameTitle";
-            this.lblGameTitle.Size = new System.Drawing.Size(110, 24);
+            this.lblGameTitle.Size = new System.Drawing.Size(163, 32);
             this.lblGameTitle.TabIndex = 0;
             this.lblGameTitle.Text = "Game Title";
             // 
             // lblUserName
             // 
             this.lblUserName.AutoSize = true;
-            this.lblUserName.Location = new System.Drawing.Point(22, 50);
+            this.lblUserName.Location = new System.Drawing.Point(33, 77);
+            this.lblUserName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblUserName.Name = "lblUserName";
-            this.lblUserName.Size = new System.Drawing.Size(35, 13);
+            this.lblUserName.Size = new System.Drawing.Size(47, 20);
             this.lblUserName.TabIndex = 1;
             this.lblUserName.Text = "By: ...";
             // 
             // lblRating
             // 
             this.lblRating.AutoSize = true;
-            this.lblRating.Location = new System.Drawing.Point(22, 70);
+            this.lblRating.Location = new System.Drawing.Point(33, 108);
+            this.lblRating.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRating.Name = "lblRating";
-            this.lblRating.Size = new System.Drawing.Size(41, 13);
+            this.lblRating.Size = new System.Drawing.Size(60, 20);
             this.lblRating.TabIndex = 2;
             this.lblRating.Text = "Rating:";
             // 
             // lblHoursPlayed
             // 
             this.lblHoursPlayed.AutoSize = true;
-            this.lblHoursPlayed.Location = new System.Drawing.Point(22, 90);
+            this.lblHoursPlayed.Location = new System.Drawing.Point(33, 138);
+            this.lblHoursPlayed.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblHoursPlayed.Name = "lblHoursPlayed";
-            this.lblHoursPlayed.Size = new System.Drawing.Size(76, 13);
+            this.lblHoursPlayed.Size = new System.Drawing.Size(107, 20);
             this.lblHoursPlayed.TabIndex = 3;
             this.lblHoursPlayed.Text = "Hours Played:";
             // 
             // txtReviewText
             // 
-            this.txtReviewText.Location = new System.Drawing.Point(25, 120);
+            this.txtReviewText.Location = new System.Drawing.Point(38, 185);
+            this.txtReviewText.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtReviewText.Multiline = true;
             this.txtReviewText.Name = "txtReviewText";
             this.txtReviewText.ReadOnly = true;
             this.txtReviewText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtReviewText.Size = new System.Drawing.Size(450, 150);
+            this.txtReviewText.Size = new System.Drawing.Size(673, 229);
             this.txtReviewText.TabIndex = 4;
             // 
             // lblReviewDate
             // 
             this.lblReviewDate.AutoSize = true;
-            this.lblReviewDate.Location = new System.Drawing.Point(350, 50);
+            this.lblReviewDate.Location = new System.Drawing.Point(525, 77);
+            this.lblReviewDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblReviewDate.Name = "lblReviewDate";
-            this.lblReviewDate.Size = new System.Drawing.Size(63, 13);
+            this.lblReviewDate.Size = new System.Drawing.Size(89, 20);
             this.lblReviewDate.TabIndex = 5;
             this.lblReviewDate.Text = "Posted on: ";
             // 
             // listReactions
             // 
             this.listReactions.HideSelection = false;
-            this.listReactions.Location = new System.Drawing.Point(25, 290);
+            this.listReactions.Location = new System.Drawing.Point(38, 446);
+            this.listReactions.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listReactions.Name = "listReactions";
-            this.listReactions.Size = new System.Drawing.Size(450, 150);
+            this.listReactions.Size = new System.Drawing.Size(673, 229);
             this.listReactions.TabIndex = 6;
             this.listReactions.UseCompatibleStateImageBehavior = false;
             this.listReactions.View = System.Windows.Forms.View.Details;
             // 
             // btnBack
             // 
-            this.btnBack.Location = new System.Drawing.Point(25, 450);
+            this.btnBack.Location = new System.Drawing.Point(38, 692);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.Size = new System.Drawing.Size(112, 35);
             this.btnBack.TabIndex = 7;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
@@ -104,19 +113,32 @@
             // 
             // btnAddReaction
             // 
-            this.btnAddReaction.Location = new System.Drawing.Point(400, 450);
+            this.btnAddReaction.Location = new System.Drawing.Point(575, 692);
+            this.btnAddReaction.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAddReaction.Name = "btnAddReaction";
-            this.btnAddReaction.Size = new System.Drawing.Size(75, 23);
+            this.btnAddReaction.Size = new System.Drawing.Size(137, 35);
             this.btnAddReaction.TabIndex = 8;
             this.btnAddReaction.Text = "Add Reaction";
             this.btnAddReaction.UseVisualStyleBackColor = true;
             this.btnAddReaction.Click += new System.EventHandler(this.btnAddReaction_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(392, 692);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 35);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Delete Review";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // ReviewDetails
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.ClientSize = new System.Drawing.Size(750, 769);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnAddReaction);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.listReactions);
@@ -126,10 +148,12 @@
             this.Controls.Add(this.lblRating);
             this.Controls.Add(this.lblUserName);
             this.Controls.Add(this.lblGameTitle);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ReviewDetails";
             this.Text = "Review Details";
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         private System.Windows.Forms.Label lblGameTitle;
@@ -141,5 +165,6 @@
         private System.Windows.Forms.ListView listReactions;
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Button btnAddReaction;
+        private System.Windows.Forms.Button button1;
     }
 }
--- APFT-113362_114143/GameShelf/Project-BD/ReviewDetails.cs
+++ APFT-113362_114143/GameShelf/Project-BD/ReviewDetails.cs
@@ -137,5 +137,89 @@ namespace Project_BD
                 LoadReviewReactions(); // Refresh reactions after adding a new one
             }
         }
+        //Delete reviw if the user vistting the page is the own that published the reaction
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult confirmResult = MessageBox.Show("Are you sure you want to delete this review?", "Confirm Deletion", MessageBoxButtons.YesNo);
+            if (confirmResult != DialogResult.Yes)
+                return;
+
+            try
+            {
+                cn = getSGBDConnection();
+                if (!verifySGBDConnection())
+                    return;
+
+                // First, check if the current user is the author of the review
+                string checkAuthorQuery = @"SELECT id_utilizador, id_jogo FROM projeto.review WHERE id_review = @reviewId";
+                SqlCommand checkCmd = new SqlCommand(checkAuthorQuery, cn);
+                checkCmd.Parameters.AddWithValue("@reviewId", reviewId);
+                SqlDataReader reader = checkCmd.ExecuteReader();
+
+                string authorId = null;
+                string gameId = null;
+
+                if (reader.Read())
+                {
+                    authorId = reader["id_utilizador"].ToString();
+                    gameId = reader["id_jogo"].ToString();
+                }
+                reader.Close();
+
+                if (authorId == null || authorId != currentUserId)
+                {
+                    MessageBox.Show("You can only delete your own reviews.");
+                    return;
+                }
+
+                // Begin delete transaction
+                SqlTransaction transaction = cn.BeginTransaction();
+
+                try
+                {
+                    // Delete reactions first
+                    SqlCommand deleteReactions = new SqlCommand(@"DELETE FROM projeto.reage_a WHERE id_review = @reviewId", cn, transaction);
+                    deleteReactions.Parameters.AddWithValue("@reviewId", reviewId);
+                    deleteReactions.ExecuteNonQuery();
+
+                    // Delete the review itself
+                    SqlCommand deleteReview = new SqlCommand(@"DELETE FROM projeto.review WHERE id_review = @reviewId", cn, transaction);
+                    deleteReview.Parameters.AddWithValue("@reviewId", reviewId);
+                    deleteReview.ExecuteNonQuery();
+
+                    // Update the game's average rating
+                    SqlCommand updateRating = new SqlCommand(@"
+                UPDATE projeto.jogo
+                SET rating_medio = (
+                    SELECT AVG(CAST(rating AS FLOAT))
+                    FROM projeto.review
+                    WHERE id_jogo = @gameId
+                )
+                WHERE id_jogo = @gameId", cn, transaction);
+                    updateRating.Parameters.AddWithValue("@gameId", gameId);
+                    updateRating.ExecuteNonQuery();
+
+                    transaction.Commit();
+
+                    MessageBox.Show("Review and its reactions deleted successfully.");
+                    this.Close(); // Close the form
+                }
+                catch (Exception innerEx)
+                {
+                    transaction.Rollback();
+                    MessageBox.Show("Error during deletion: " + innerEx.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error deleting review: " + ex.Message);
+            }
+            finally
+            {
+                if (cn != null && cn.State == ConnectionState.Open)
+                    cn.Close();
+            }
+        }
+
     }
 }
